137a88b61c7283f371934f627d36c0c4010f5677
benniismael1 <83157018+benniismael1@users.noreply.github.com>
2021-04-26 10:21:25 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1574,7 +1574,7 @@ router.get('/randomanime', async (req, res, next) => {
 	if(!apikeyInput) return res.json(loghandler.notparam)
 	if(apikeyInput != 'beniismael') return res.json(loghandler.invalidKey)
 
-       fetch(encodeURI(`https://alpin-api-2021.herokuapp.com/api/anime/random?apikey=${alpin1}`))
+       fetch(encodeURI(`https://alpin-api-2021.herokuapp.com/api/anime/random?apikey=alpin1`))
         .then(response => response.json())
         .then(data => {
         var result = data;
